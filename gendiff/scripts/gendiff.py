80802d8990176de7080d6f45d24498526b3d584e
Juliana <julianacarvajalguerra@gmail.com>
2026-04-13 12:32:11 -0500

    Add generate_diff function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,26 @@
 import argparse
+import json
+from pathlib import Path
+
+def generate_diff(file1, file2):
+
+    first_file = json.load(open(file1))
+    second_file = json.load(open(file2))
+    
+    deleted_keys = set(first_file.keys()) - set(second_file.keys())
+    added_keys = set(second_file.keys()) - set(first_file.keys())
+    shared_keys = set(first_file.keys()) & set(second_file.keys())
+    for k in shared_keys:
+        if first_file[k] != second_file[k]:
+            deleted_keys.add(k)
+            added_keys.add(k)
+
+    output = {f"- {k}": first_file[k] for k in sorted(deleted_keys)} | {f"+ {k}": second_file[k] for k in sorted(added_keys)}
+
+    print(output)
+    #print(json.dumps(output, indent=4))
+    # for k, v in output.items():
+    #     print(f"{k}: {v}")
 
 def main():
     parser = argparse.ArgumentParser(
@@ -9,6 +31,8 @@ def main():
     parser.add_argument('-f', '--format', help="set format of output")
 
     args = parser.parse_args()
+    
+    generate_diff(args.first_file, args.second_file)
 
 if __name__ == "__main__":
     main()
